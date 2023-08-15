c368f7e07792c18dbb6223673c619e13aa513163
HenriqueMAlves <m.henriquealves@gmail.com>
2023-08-14 22:17:00 -0300

    test: test generic interface

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from src.rsa import RSA
+# from src.services.rsa import RSA
 
-service = RSA(17, 23)
+# service = RSA(17, 23)
 
-message = "morte ao miojo"
-print("+ original message: ", message)
+# message = "morte ao miojo"
+# print("+ original message: ", message)
 
-coded = service.code(message)
-print("+ message coded: ", coded)
+# coded = service.code(message)
+# print("+ message coded: ", coded)
 
-decoded = service.decode(coded)
-print("+ message decoded: ", decoded)
+# decoded = service.decode(coded)
+# print("+ message decoded: ", decoded)
+
+from src.interface.screen import GenericScreen
+
+
+screen: GenericScreen = GenericScreen()
+
+input = screen.input()
+button = screen.button()
+label = screen.label()
+
+screen.run()
